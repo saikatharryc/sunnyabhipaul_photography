f5662f50ed075886d2da3e8135586ac57ea49778
Saikat Chakrabortty <saikatchakrabortty2@gmail.com>
2016-08-22 19:13:05 +0530

    updated welcome message .user_guide problem removed

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -76,7 +76,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		<p>If you would like to edit this page you'll find it located at:</p>
 		<code>application/views/welcome_message.php</code>
 
-		<p>The corresponding controller for this page is found at:</p>
+		<p>The corresponding controller for this page is found at: hsdggssjdh</p>
 		<code>application/controllers/Welcome.php</code>
 
 
